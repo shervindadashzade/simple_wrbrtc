9d6d75bf09fec613692513fc627f8d873d15e65c
shervin dadashzade <shervin.dadashzade7988@gmail.com>
2021-02-18 12:18:42 +0330

    simple example completed

---
+++ client.js
@@ -0,0 +1,58 @@
+const myVideo = document.getElementById('myVideo');
+const remoteVideo = document.getElementById('remoteVideo');
+const myId = document.getElementById('myId');
+const callBtn = document.getElementById('call');
+const hangoutBtn = document.getElementById('hangout');
+const remoteId = document.getElementById('remoteId');
+const peer = new Peer();
+var isCalle = false;
+var connection = null;
+var call_confirm;
+var stream;
+const constraints = {
+    audio : true,
+    video : true
+};
+
+
+
+async function init(){
+    try{
+        stream = await navigator.mediaDevices.getUserMedia(constraints);
+        myVideo.srcObject = stream;
+    }catch(e){
+        console.log(e);
+    }
+}
+init();
+
+peer.on('open',(id) =>{
+    myId.innerHTML = `your id is : ${id}`;
+})
+
+callBtn.addEventListener('click',() => {
+    if(remoteId.value){
+        console.log('connection is establishing');
+        call = peer.call(remoteId.value,stream);
+        call.on('stream',function(stream){
+            remoteVideo.srcObject = stream;
+        })
+    }else{
+        alert("please enter an id");
+    }
+})
+
+peer.on('call',function(call){
+    call_confirm = confirm('you have a call, do you wanna answer that?');
+    isCalle = true;
+    callMedia = call;
+
+    call.on('stream',function(stream){
+        remoteVideo.srcObject = stream;
+    })
+
+    if(call_confirm){
+        call.answer(stream);
+    }
+})
+
